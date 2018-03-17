0eea47c67e23ddf68b168a802696a67e30b6faf0
MaryHall17 <mh6uj@students-MBP.home>
2018-03-16 21:35:55 -0400

    trying to get id to work

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,51 +1,65 @@
 $(function() {
 	$("#article").on("click", function(event) {
-		
+		var id = $(this).data("id");
+		var article = {
+			title: $(this).data("title"),
+			summary: $(this).data("summary"),
+			link: $(this).data("saved")
+		};
+
+		$.ajax("/saved/" + id, {
+			type: "POST",
+			data: article
+		}).then(
+			function () {
+				console.log("article was saved");
+			})
 	})
 }
 
 
 
-// $(document).on("click", "p", function() {
-// 	$("#notes").empty();
+$(document).on("click", "p", function() {
+	$("#notes").empty();
 
-// 	var thisId = $(this).attr("data-id");
+	var thisId = $(this).attr("data-id");
 
-// 	$.ajax({
-// 		method: "GET",
-// 		url: "/headlines/" + thisId
-// 	})
-// 	.then(function(data) {
-// 		console.log(data);
-// 		$("#notes").append("<h2>" + data.title + "</h2>");
-// 		$("#notes").append("<input id='titleinput' name='title >");
-// 		$("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-// 		$("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+	$.ajax({
+		method: "GET",
+		url: "/headlines/" + thisId
+	})
+	.then(function(data) {
+		console.log(data);
+		$("#notes").append("<h2>" + data.title + "</h2>");
+		$("#notes").append("<input id='titleinput' name='title >");
+		$("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+		$("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-// 		if (data.note) {
-// 			$("#titleinput").val(data.note.title);
-// 			$("#bodyinput").val(data.note.body);
-// 		}
-// 	});
-// });
+		if (data.note) {
+			$("#titleinput").val(data.note.title);
+			$("#bodyinput").val(data.note.body);
+		}
+	});
+});
 
-// //Save note button
-// $(document).on("click", "#savenote", function() {
-// 	var thisId = $(this).attr("data-id");
+//Save note button
+$(document).on("click", "#savenote", function() {
+	var thisId = $(this).attr("data-id");
+	console.log("button clicked");
 
-// 	$.ajax({
-// 		method: "POST",
-// 		url: "/headlines/" + thisId,
-// 		data: {
-// 			title: $("#titleinput").val(),
-// 			body: $("#bodyinput").val(),
-// 		}
-// 	})
-// 		.then(function(data) {
-// 			console.log(data);
-// 			$("#notes").empty();
-// 		});
+	$.ajax({
+		method: "POST",
+		url: "/headlines/" + thisId,
+		data: {
+			title: $("#titleinput").val(),
+			body: $("#bodyinput").val(),
+		}
+	})
+		.then(function(data) {
+			console.log(data);
+			$("#notes").empty();
+		});
 
-// 		$("#titleinput").val("");
-// 		$("#bodyinput").val("");
-// });
+		$("#titleinput").val("");
+		$("#bodyinput").val("");
+});
